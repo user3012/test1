65fcfe1b58a4706c84860001a70661a80e317c6a
user3012 <60866777+user3012@users.noreply.github.com>
2020-11-01 11:18:44 +0800

    Add files via upload

---
+++ level1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class level1 {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+    if (input % 2 == 0){
+        System.out.println(true);
+    } else {
+        System.out.println(false);
+    }
+
+
+
+    }
+
+
+
+}
