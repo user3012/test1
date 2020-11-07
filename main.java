9e75afdf954e4c1ae60888018133c57b5f71e798
user3012 <60866777+user3012@users.noreply.github.com>
2020-11-07 20:31:06 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        aniaml jessi = new cat();
+//        jessi.setAge(12);
+//        jessi.setName("jessi");
+//        jessi.setSex("female");
+        jessi.eatig();
+        jessi.sleeping();
+         doge Bob = new doge();
+//         Bob.setName("Bob");
+//         Bob.setAge(9);
+//         Bob.setSex("male");
+         Bob.sleeping();
+         Bob.run();
+         Bob.run(10);
+         Bob.run('!');
+    }
+}
