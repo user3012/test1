8a3469cf8b64039126f5cbba02defc0e164797bb
user3012 <60866777+user3012@users.noreply.github.com>
2020-11-15 13:25:58 +0800

    Add files via upload

---
+++ main4.java
@@ -0,0 +1,11 @@
+public class main4 {
+
+    public static void main(String[] args) {
+         add4 add4 = new add4(20,10);
+         substract4 substract4 = new substract4(53,24);
+         multiply4 multiply4 = new multiply4(11,12);
+         divide4 divide4 =new divide4(24,8);
+           System.out.println();
+
+    }
+}
